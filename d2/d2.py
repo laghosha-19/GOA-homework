9dab44476630d02d4db674a78318cf5062479f59
laghosha-19 <laghoshvili.luka@gmail.com>
2024-10-11 21:44:06 +0400

    second commit

---
+++ d2/d2.py
@@ -0,0 +1,19 @@
+##so lets build robot "matxovari"
+
+print("Hello welcome to lagoshas restourant!!!!")
+
+name = input("Wahts is ur name sir? \n")
+
+print("Hello "+ name +  ", Thank you so much for coming in today." )
+
+#what we got in menu
+
+menu = "Shawarma, Xinkali, Pilmeni, Xachapuri,Lobiani,Sacivi,Baje."
+menu(drinks = "Nataxtari, Cola, Fanta, Fanta propic, Boom fire, Pepsi." 
+
+
+
+
+
+
+
