d29bc3dfbd30562f2e88d4ee854479761bc4cbc9
laghosha-19 <laghoshvili.luka@gmail.com>
2024-10-22 20:46:37 +0400

    classwork python

---
+++ classworkpy.py
@@ -0,0 +1,51 @@
+print (10 + 5)
+print (10 + 6)
+print (10 + 7)
+print (10 + 8)
+print(10 + 9)
+
+
+print (10 -5)
+print(10 - 6)
+print(10 - 7)
+print(10 - 8)
+print(10 - 9)
+
+print(10 * 2)
+print(10 * 3)
+print(10 * 4)
+print(10 * 5)
+print(10 * 6)
+
+print(10 / 2)
+print(10 / 5)
+print(20 /10)
+print(20 / 2)
+print(20 / 5)
+
+print(10 % 3)
+print(10 % 4)
+print(10 % 6)
+print(10 % 7)
+print (10 % 8)
+
+print(5 ** 2)
+print(5 ** 3)
+print(5 ** 4)
+print(5 ** 5)
+print(5 * 6)
+
+
+
+
+
+
+
+num1 = 5
+num2 = 5
+num3 = 2
+num4 = 2
+num5 = 10
+num6 = 2
+
+print(num1 + num2 - num2 * num3 % num4 / num5 ** num6)
