2f1865e0ae92593027bd08d3aa54c8ddfb7639c0
laghosha-19 <laghoshvili.luka@gmail.com>
2024-10-22 21:55:22 +0400

    classwork 10.22.2024

---
+++ classwork.py
@@ -0,0 +1,81 @@
+num1 = 10
+num1 = num1 + 5
+print (num1)
+
+num1 = 10
+num1 = num1 + 6
+print (num1)
+
+num1 = 10
+num1 = num1 + 7
+print (num1)
+
+num1 = 10
+num1 = num1 + 8
+print (num1)
+
+num1 = 10
+num1 = num1 + 9
+print (num1)
+
+
+
+num2 = 10
+num2 = num2 - 5
+print (num2)
+
+num2 = 10
+num2 = num2 - 4
+print (num2)
+
+num2 = 10
+num2 = num2 - 3
+print (num2)
+
+num2 = 10
+num2 = num2 - 2
+print (num2)
+
+num2 = 10
+num2 = num2 - 1
+print (num2)
+
+
+num3 = 10
+num3 = num3 / 2
+print (num3)
+
+num3 = 10
+num3 = num3 / 3
+print (num3)
+
+num3 = 10
+num3 = num3 / 4
+print (num3)
+
+num3 = 10
+num3 = num3 /5
+print (num3)
+
+num3 = 10
+num3 = num3 / 6
+
+num4 = 10
+num4 = num4 * 2
+print (num4)
+
+num4 = 10
+num4 = num4 * 4
+print (num4)
+
+num4 = 10
+num4 = num4 * 5
+print (num4)
+
+num4 = 10
+num4 = num4 * 6
+print (num4)
+
+num4 = 10
+num4 = num4 * 7
+print (num4)
